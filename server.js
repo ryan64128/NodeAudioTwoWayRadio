24638f3c6cba03e076f990ca11c1342c16e14914
Ryan Roberts <ryan64128@yahoo.com>
2020-05-31 22:25:47 -0400

    port fix

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var http = require('http');
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8010;
 
 var app = express()
 			.use(express.static(__dirname + '/public'))
-			.listen(8080);
+			.listen(port);
 
 // http.createServer(app).listen(8080);
+console.log("server.js running on port " + port);
