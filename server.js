df61b40c13315e9f3c51c27c71a6c64f90619515
Ryan Roberts <ryan64128@yahoo.com>
2020-05-31 22:17:59 -0400

    Added port changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var http = require('http');
 
+var port = process.env.PORT || 3000;
+
 var app = express()
 			.use(express.static(__dirname + '/public'))
 			.listen(8080);
