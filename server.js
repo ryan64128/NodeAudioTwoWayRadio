53058ddda0f11bd5f46357b2af9f595c51741190
Ryan Roberts <ryan64128@yahoo.com>
2020-06-02 19:39:47 -0400

    added radio feedback noise

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var http = require('http');
+var nodeMailer = require('nodemailer');
 
 var port = process.env.PORT || 8010;
 
@@ -9,3 +10,26 @@ var app = express()
 
 // http.createServer(app).listen(8080);
 console.log("server.js running on port " + port);
+
+var transporter = nodeMailer.createTransport({
+	service: 'Gmail',
+	auth: {
+		user: 'ryan64128@gmail.com',
+		pass: 'Ry64An128'
+	}
+});
+
+var mailOptions = {
+	from: 'ryan64128@gmail.com',
+	to: 'fatima64128@yahoo.com',
+	subject: 'Sending e-mail using Node.js',
+	text: 'Hello World, nodemailer!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+	if (error){
+		console.log(error);
+	} else {
+		console.log('Email sent: ' + info.response);
+	}
+});
